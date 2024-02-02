d537d7de230f659630205d223b0ce488e13bf57f
SEERRGEE <ieg.sergo@gmail.com>
2024-02-02 16:02:14 -0500

    add contact section

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import GithubIcon from '../../public/github.svg'
+import MastodonIcon from '../../public/mastodon.svg'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const EmailSection = () => {
+  return (
+    <section className='grid md:grid-cols-2 my-12 md:py-24 gap-4 relative'>
+        <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -traslate-x-1/2 -translate-1/2'>
+            {/* Purple Sphere */}
+        </div>
+      <div className="z-10">
+        <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+            We are constantly looking for new opportunities! Our inbox is always open. Whether you have a question or just want to say hi, we will do our best to respond to you as quickly as possible!
+        </p>
+        <div className='social-links flex flex-row gap-2'>
+            <Link href={'github.com'}>
+                <Image  src={GithubIcon} alt='github icon' width={40} height={40}/>
+            </Link>
+            <Link href={'mastodon.social'}>
+                <Image  src={MastodonIcon} alt='mastodon icon' width={50} height={50}/>
+            </Link>
+        </div>
+      </div>
+      <div>
+        <form className='flex flex-col gap-4'>
+            <div className='mb-2'>
+                <label htmlFor="email" className='text-white block text-sm font-medium py-1'>
+                    Email:
+                </label>
+                <input type={'email'} placeholder='jon@gmail.com' name="email" id="email" required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'/>
+            </div>
+            <div className='mb-2'>
+                <label htmlFor="subject" className='text-white block text-sm font-medium py-1'>
+                    Subject:
+                </label>
+                <input type={'text'} placeholder='I need a website' name="subject" id="subject" required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'/>
+            </div>
+            <div className='mb-6'>
+                <label htmlFor='message' className='text-white block text-sm py-1 font-medium'>
+                    Message:
+                </label>
+                <textarea id='message' name='message' placeholder="Hi, Serge I would like to talk about..." className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'>
+                </textarea>
+            </div>
+            <button type='submit' className='-mt-4 bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default EmailSection
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import AboutSection from "./components/AboutSection";
+import EmailSection from "./components/EmailSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import ProjectsSection from "./components/ProjectsSection";
@@ -12,6 +13,7 @@ export default function Home() {
       <HeroSection/>
       <AboutSection/>
       <ProjectsSection/>
+      <EmailSection/>
     </div>
   </main>
   );
